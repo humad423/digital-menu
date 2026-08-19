c90167294cca46f9a1261ed3e13a77af936b098e
HUSAM <humad423@gmail.com>
2026-08-19 09:11:48 +0300

    fix(offers): include max_offers_limit in restaurant-panel fetch query

--- src/app/restaurant-panel/[id]/page.tsx
+++ src/app/restaurant-panel/[id]/page.tsx
@@ -206,7 +206,7 @@ export default function RestaurantOwnerPanel({ params }: { params: Promise<{ id:
           'id, name, slug, primary_color, whatsapp_number, logo_url, cover_url, ' +
           'latitude, longitude, delivery_radius_km, delivery_tiers, has_delivery, ' +
           'enable_whatsapp_orders, is_on_holiday, opening_time, closing_time, days_off, store_type, ' +
-          'is_subscription_active, is_menu_active, subscription_notes'
+          'max_offers_limit, is_subscription_active, is_menu_active, subscription_notes'
         )
         .eq('id', id)
         .maybeSingle() as any
